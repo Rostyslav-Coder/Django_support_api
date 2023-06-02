c3654bdf614c6de5864c32ae9724870a5cdca068
Rostyslav <rostyslavputnikov@gmail.com>
2023-06-02 23:41:20 +0300

    📝 Added Tables Request & Message

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 
-class Users(models.Model):
+class User(models.Model):
     """Class to create Users Table"""
     username = models.CharField(max_length=100)
     email = models.EmailField()
@@ -12,5 +12,30 @@ class Users(models.Model):
     role = models.PositiveSmallIntegerField()
 
     class Meta:
-        """Meta class to rename users table"""
+        """Class Meta to rename users table"""
         db_table = "users"
+
+
+class Request(models.Model):
+    """Class to create Request Table"""
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    visibility = models.BooleanField(default=True)
+    status = models.PositiveSmallIntegerField()
+    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="user_requests")
+    manager = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="manager_requests")
+
+    class Meta:
+        """Class Meta to rename request table"""
+        db_table = "requests"
+
+
+class Message(models.Model):
+    """Class to create Message Table"""
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="messages")
+    request = models.ForeignKey(Request, on_delete=models.RESTRICT, related_name="messages")
+
+    class Meta:
+        """Class Meta to rename message table"""
+        db_table = "messages"
