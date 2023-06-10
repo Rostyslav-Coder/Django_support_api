eed4a8c444095944adb35330bebf0d81f5013a63
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2023-06-10 16:16:06 +0300

    🔨 Project refurbished

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.users import create_user
+from core.api import create_user
 
 urlpatterns = [
     path("admin/", admin.site.urls),
