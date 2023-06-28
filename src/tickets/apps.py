f8fca8b3e0a765cb54cf2d102f153dea4cb21c9a
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2023-06-28 16:37:29 +0300

    ✅ Module docstring added

--- src/tickets/apps.py
+++ src/tickets/apps.py
@@ -1,6 +1,10 @@
+"""This is module for configuration app in Ticket component."""
+
 from django.apps import AppConfig
 
 
 class TicketsConfig(AppConfig):
+    """A class that configurat Tickets."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "tickets"
--- src/tickets/models.py
+++ src/tickets/models.py
@@ -1,16 +1,18 @@
-"""This is module to create Tickets & Messages DB Table with fields."""
+"""This is module to creating Tickets & Message DB Tabl in Ticket component."""
 
 from django.conf import settings
 from django.db import models
 
+from tickets.constants import TicketStatus
+
 
 class Ticket(models.Model):
-    """Class to create Request Table"""
+    """Class to create Ticket BD Table."""
 
     title = models.CharField(max_length=100)
     text = models.TextField()
     visibility = models.BooleanField(default=True)
-    status = models.PositiveSmallIntegerField()
+    status = models.PositiveSmallIntegerField(default=TicketStatus.NOT_STARTED)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.RESTRICT,
@@ -20,16 +22,17 @@ class Ticket(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.RESTRICT,
         related_name="manager_tickets",
+        null=True,
     )
 
     class Meta:
-        """Class Meta to rename tickets table"""
+        """Class Meta to rename tickets table."""
 
         db_table = "tickets"
 
 
 class Message(models.Model):
-    """Class to create Message Table"""
+    """Class to create Message BD Table."""
 
     text = models.TextField()
     user = models.ForeignKey(
@@ -42,6 +45,6 @@ class Message(models.Model):
     )
 
     class Meta:
-        """Class Meta to rename message table"""
+        """Class Meta to rename message table."""
 
         db_table = "messages"
