8ad2748e2fa2d1248f25a83f4d98f49d8484a2d6
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2023-06-28 16:34:42 +0300

    ✅ Module docstring added

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,4 +1,4 @@
-"""URL configuration for config authentication."""
+"""This is module for configuration URL in Authentication component"""
 
 from django.urls import path
 from rest_framework_simplejwt.views import token_obtain_pair
